1335dea82a2c2e17b537c8b8677408bf34ef9047
itarek99 <tarekulislam7060@gmail.com>
2022-12-10 22:59:54 +0600

    formspree implement

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Toaster } from 'react-hot-toast';
 import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <>
       <Home />
+      <Toaster />
     </>
   );
 }
--- src/Pages/Home/Components/Contact.js
+++ src/Pages/Home/Components/Contact.js
@@ -1,4 +1,17 @@
+import { useForm, ValidationError } from '@formspree/react';
+import { toast } from 'react-hot-toast';
+
 const Contact = () => {
+  const [state, handleSubmit] = useForm(process.env.REACT_APP_FORMSPREE_ENDPOINT);
+  if (state.succeeded) {
+    toast.success('Your Message Was Sent Successfully.', {
+      style: {
+        background: '#2A2C39',
+        color: '#fff',
+      },
+    });
+  }
+
   return (
     <div id='contact' className='container mx-auto px-2 py-20'>
       <div>
@@ -12,14 +25,21 @@ const Contact = () => {
       </div>
 
       <div className='mt-14'>
-        <form action='' className='w-full max-w-5xl mx-auto'>
+        <form onSubmit={handleSubmit} className='w-full max-w-5xl mx-auto'>
           <div className='grid grid-cols-2 gap-6'>
-            <input type='text' placeholder='Name' className='input input-bordered w-full' />
-            <input type='email' placeholder='Email' className='input input-bordered w-full' />
-            <textarea rows='5' className='textarea textarea-bordered col-span-2' placeholder='Message'></textarea>
+            <input type='text' placeholder='Name' name='name' className='input input-bordered w-full' />
+            <input type='email' placeholder='Email' name='email' className='input input-bordered w-full' />
+            <ValidationError prefix='Email' field='email' errors={state.errors} />
+            <textarea
+              rows='5'
+              name='message'
+              placeholder='Message'
+              className='textarea textarea-bordered col-span-2'
+            ></textarea>
+            <ValidationError prefix='Message' field='message' errors={state.errors} />
           </div>
           <div className='text-right mt-6'>
-            <button className='btn btn-outline font-medium text-sm' type='submit'>
+            <button className='btn btn-outline font-medium text-sm' type='submit' disabled={state.submitting}>
               SEND MESSAGE
             </button>
           </div>
