9f32b34b66056d55421ff8e2b9d82524a82ba4a8
itarek99 <tarekulislam7060@gmail.com>
2022-12-10 22:17:31 +0600

    footer complete

--- src/Navigation/Footer.js
+++ src/Navigation/Footer.js
@@ -1,4 +1,23 @@
+import { FaFacebookSquare, FaGithubSquare, FaLinkedin } from 'react-icons/fa';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className='bg-[#2A2C39]'>
+      <div className='container mx-auto px-2 py-6'>
+        <p className='text-lg'>Social Connections</p>
+        <div className='flex gap-2 text-3xl mt-2'>
+          <a href='f.com'>
+            <FaFacebookSquare />
+          </a>
+          <a href='f.com'>
+            <FaLinkedin />
+          </a>
+          <a href='f.com'>
+            <FaGithubSquare />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Footer;
