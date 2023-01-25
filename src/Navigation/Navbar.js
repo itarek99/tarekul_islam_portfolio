49925479e577794f0d20a5c9b1585883c16098b2
itarek99 <tarekulislam7060@gmail.com>
2023-01-26 00:47:57 +0600

    mobile nav added

--- src/Navigation/Navbar.js
+++ src/Navigation/Navbar.js
@@ -7,7 +7,35 @@ const Navbar = () => {
             Tarek
           </a>
         </div>
-        <div className='flex-none'>
+
+        {/* small screen navbar */}
+        <div className='block md:hidden fixed text-center px-2 w-full left-0 right-0 top-20 z-40'>
+          <ul className='mx-auto w-full max-w-xl bg-slate-400 space-y-3 py-4'>
+            <li>
+              <a className='py-1 block' href='#projects'>
+                Projects
+              </a>
+            </li>
+            <li>
+              <a className='py-1 block' href='#about'>
+                About
+              </a>
+            </li>
+            <li>
+              <a className='py-1 block' href='#contact'>
+                Contact
+              </a>
+            </li>
+            <li>
+              <a className='py-1 block' href='/#blog'>
+                Blog
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        {/* big screen navbar */}
+        <div className='hidden md:block flex-none'>
           <ul className='menu menu-horizontal px-1'>
             <li>
               <a href='#projects'>Projects</a>
